4cadca46150bd1f3214759bcc23c000bc6cc180b
trungkien2k5 <kienupsu2@gmail.com>
2026-08-29 22:43:20 +0700

    feat(restaurant): enable caching and JPA auditing

--- restaurant-service/src/main/java/com/kien/restaurant/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/kien/restaurant/RestaurantServiceApplication.java
@@ -2,12 +2,15 @@ package com.kien.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 public class RestaurantServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
 }
