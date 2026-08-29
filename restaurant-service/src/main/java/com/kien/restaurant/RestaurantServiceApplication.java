0148ac825a2c50c4020522c0949cc96bffe5d377
trungkien2k5 <kienupsu2@gmail.com>
2026-08-29 22:46:15 +0700

    feat(restaurant): enable method-level role authorization

--- restaurant-service/src/main/java/com/kien/restaurant/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/kien/restaurant/RestaurantServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableCaching
 @EnableJpaAuditing
+@EnableMethodSecurity
 @SpringBootApplication
 public class RestaurantServiceApplication {
 
