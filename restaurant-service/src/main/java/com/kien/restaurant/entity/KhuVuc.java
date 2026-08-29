674a743598fabefdbb4a7284d43b5b5eada4b442
trungkien2k5 <kienupsu2@gmail.com>
2026-08-29 22:43:12 +0700

    feat(restaurant): add audit fields to KhuVuc

--- restaurant-service/src/main/java/com/kien/restaurant/entity/KhuVuc.java
+++ restaurant-service/src/main/java/com/kien/restaurant/entity/KhuVuc.java
@@ -1,4 +1,5 @@
 package com.kien.restaurant.entity;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -8,7 +9,7 @@ import java.util.List;
 @Entity
 @Table(name = "KhuVuc")
 @Data
-public class KhuVuc {
+public class KhuVuc extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer maKhuVuc;
