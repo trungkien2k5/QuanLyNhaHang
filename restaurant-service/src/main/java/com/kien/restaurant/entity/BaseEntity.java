e0edafaf04026402ebc1c7d44b908a3ea3ef71b2
trungkien2k5 <kienupsu2@gmail.com>
2026-08-29 22:43:00 +0700

    feat(restaurant): add JPA audit base entity

---
+++ restaurant-service/src/main/java/com/kien/restaurant/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.kien.restaurant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+}
