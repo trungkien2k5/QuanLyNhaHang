4cb06d5bd95cd900c61b1bdf50f5b8ba5ab6f95f
trungkien2k5 <kienupsu2@gmail.com>
2026-08-29 22:43:08 +0700

    feat(restaurant): add audit fields to Ban

--- restaurant-service/src/main/java/com/kien/restaurant/entity/Ban.java
+++ restaurant-service/src/main/java/com/kien/restaurant/entity/Ban.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kien.restaurant.entity.enums.TrangThaiBan;
 import jakarta.persistence.*;
 import lombok.Data;
+
 @Entity
 @Table(name = "Ban")
 @Data
-public class Ban {
+public class Ban extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer maBan;
